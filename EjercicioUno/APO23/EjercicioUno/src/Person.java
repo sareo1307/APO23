eb26d56e9cb56f6426bb3d338549b92952e41d0c
sareo1307 <125764987+sareo1307@users.noreply.github.com>
2023-02-19 19:11:23 -0500

    commi 3

---
+++ EjercicioUno/APO23/EjercicioUno/src/Person.java
@@ -0,0 +1,36 @@
+
+public class Person {
+
+	private String name;
+	private int edad;
+	
+	
+	public Person() {
+		
+		
+		
+	}
+
+	public Person(String name, int edad) {
+		
+		this.name = name;
+		this.edad = edad;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
+	
+}
