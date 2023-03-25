4bbc634b3d513924ea6c1ea3d442882d61fab809
Arpra4586 <supreme_4586@hotmail.com>
2023-03-25 16:30:21 -0500

    commit 1

---
+++ Taller6/Taller6/src/PuntoCuatro.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PuntoCuatro {
+	
+	static Scanner sn = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+	
+	int[] numeros = new int [10];
+	
+	numeros[0] = 1;
+	numeros[1] = 1;
+	numeros[2] = 1;
+	numeros[3] = 1;
+	numeros[4] = 1;
+	numeros[5] = 1;
+	numeros[6] = 1;
+	numeros[7] = 1;
+	numeros[8] = 1;
+	numeros[9] = 1;
+ 
+	for (int i = 0; i < numeros.length; i++){
+			
+	System.out.println(numeros[i]);
+	
+	}
+ }	
+}
---
+++ Taller6/Taller6/src/PuntoDos.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PuntoDos {
+	
+static Scanner sn = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+	
+	float[][] precio;
+	
+	precio = new float [10][3];
+	
+for(float j = 0; j < precio.length; j++){
+			
+	System.out.println("el precio es igual " + j + " dolares ");
+		
+   }
+  }
+ }
